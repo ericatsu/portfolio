8733126411a267fce7e872e60352b186d389692a
ericatsu <ekaatsu@st.ug.edu.gh>
2021-07-08 16:18:05 +0000

    Add route and switch to app.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-// import About from './Components/About';
-// import Footer from './Components/Footer';
-// import Home from './Components/Home';
-import NavBar from './Components/NavBar';
-// import Skills from './Components/Skills';
-// import Works from './Components/Works'
-// import Contact from './Components/Contact'
-import SideBar from './Components/SideBar';
+import HireMe from './Components/HireMe';
 import Home from './pages/Home';
 
 
@@ -15,16 +8,11 @@ import Home from './pages/Home';
 function App() {
   return (
     <Router>
-          <Home/>
-        {/* <Switch>
+        <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/About" component={About} />
-          <Route exact path="/Skills" component={Skills} />
-          <Route exact path="/Works" component={Works} />
-          <Route exact path="/Contact" component={Contact} />
+          <Route exact path="/HireMe" component={HireMe} />
         </Switch>
-        <Works/>
-        <Footer/> */}
+        {/* <Footer/> */}
     </Router>
   );
 }
---
+++ src/Components/HireMe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HireMe = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HireMe
