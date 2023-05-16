2df4ed9ace4b0f728ce658aff4ee54731c20fa6b
ericatsu <ekaatsu@st.ug.edu.gh>
2023-05-16 18:25:51 +0000

    ✅ Nav bar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,126 @@
 import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
+import { CloseIcon, HeaderWrapper, HireMeButton, Logo, MenuIcon, MobileMenu, MobileMenuButton, MobileMenuItem, NavItem, NavMenu } from '../elements/HeaderElements';
 
-function Header({ openHireMeDialog }) {
+
+const Header = ({ openHireMeDialog }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
-    <header>
-      {/* Your header content */}
-      <button onClick={openHireMeDialog}>Hire Me</button>
-    </header>
-  );
-}
+    <HeaderWrapper>
+      <Logo onClick={() => scroll.scrollToTop()}>
+        EA
+      </Logo>
+      <NavMenu>
+        <NavItem
+                to="about"
+      spy={true}
+      smooth={true}
+      duration={500}
+      offset={-60}
+      activeClass="active"
+      onClick={closeMobileMenu}
+    >
+      About
+    </NavItem>
+    <NavItem
+      to="skills"
+      spy={true}
+      smooth={true}
+      duration={500}
+      offset={-60}
+      activeClass="active"
+      onClick={closeMobileMenu}
+    >
+      Skills
+    </NavItem>
+    <NavItem
+      to="projects"
+      spy={true}
+      smooth={true}
+      duration={500}
+      offset={-60}
+      activeClass="active"
+      onClick={closeMobileMenu}
+    >
+      Projects
+    </NavItem>
+    <NavItem
+      to="contact"
+      spy={true}
+      smooth={true}
+      duration={500}
+      offset={-60}
+      activeClass="active"
+      onClick={closeMobileMenu}
+    >
+      Contact
+    </NavItem>
+  </NavMenu>
+  <HireMeButton onClick={openHireMeDialog}>  Hire Me</HireMeButton>
+  <MobileMenuButton onClick={toggleMobileMenu}>
+    {isMobileMenuOpen ? <CloseIcon /> : <MenuIcon />}
+  </MobileMenuButton>
+  {isMobileMenuOpen && (
+    <MobileMenu>
+      <MobileMenuItem
+        to="about"
+        spy={true}
+        smooth={true}
+        duration={500}
+        offset={-60}
+        activeClass="active"
+        onClick={closeMobileMenu}
+      >
+        About
+      </MobileMenuItem>
+      <MobileMenuItem
+        to="skills"
+        spy={true}
+        smooth={true}
+        duration={500}
+        offset={-60}
+        activeClass="active"
+        onClick={closeMobileMenu}
+      >
+        Skills
+      </MobileMenuItem>
+      <MobileMenuItem
+        to="projects"
+        spy={true}
+        smooth={true}
+        duration={500}
+        offset={-60}
+        activeClass="active"
+        onClick={closeMobileMenu}
+      >
+        Projects
+      </MobileMenuItem>
+      <MobileMenuItem
+        to="contact"
+        spy={true}
+        smooth={true}
+        duration={500}
+        offset={-60}
+        activeClass="active"
+        onClick={closeMobileMenu}
+      >
+        Contact
+      </MobileMenuItem>
+    </MobileMenu>
+  )}
+</HeaderWrapper>
+);
+};
 
 export default Header;
+
+
---
+++ src/elements/HeaderElements.jsx
@@ -0,0 +1,113 @@
+import styled from 'styled-components';
+import { Link } from 'react-scroll';
+import { RiMenuLine, RiCloseLine } from 'react-icons/ri';
+
+export const HeaderWrapper = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #fff;
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 0 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export const Logo = styled.div`
+  font-size: 28px;
+  font-weight: bold;
+  color: #333;
+  cursor: pointer;
+`;
+
+export const NavMenu = styled.nav`
+  display: flex;
+  align-items: center;
+  padding-right: 20px;
+`;
+
+export const NavItem = styled(Link)`
+  margin-right: 30px;
+  color: #333;
+  cursor: pointer;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #ff6b6b;
+  }
+
+  &.active {
+    color: #ff6b6b;
+  }
+`;
+
+export const HireMeButton = styled.button`
+  background-color: #ff6b6b;
+  color: #fff;
+  border: none;
+  padding: 8px 16px;
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #ff4f4f;
+  }
+`;
+
+export const MobileMenuButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  display: none;
+  outline: none;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+export const CloseIcon = styled(RiCloseLine)`
+  font-size: 24px;
+`;
+
+export const MenuIcon = styled(RiMenuLine)`
+  font-size: 24px;
+`;
+
+export const MobileMenu = styled.div`
+  position: fixed;
+  top: 60px;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const MobileMenuItem = styled(Link)`
+  margin-bottom: 10px;
+  color: #333;
+  cursor: pointer;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #ff6b6b;
+  }
+
+  &.active {
+    color: #ff6b6b;
+  }
+`;
