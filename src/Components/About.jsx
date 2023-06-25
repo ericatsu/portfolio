610cf9da83e801340fe970cbd37abcb743c50883
ericatsu <ekaatsu@st.ug.edu.gh>
2023-06-25 23:31:48 +0000

    ✨

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,39 +1,59 @@
-import React from 'react'
+import React from 'react';
 import { FiSmartphone, RiGlobeFill, RiPencilFill } from 'react-icons/all';
-import { AboutSection, ArrowIcon, Card, CardIcon, CardText, DiscoverMore } from '../elements/AboutElements';
+import {
+  AboutSection,
+  LeftContainer,
+  RightContainer,
+  Title,
+  Description,
+  NumberWrapper,
+  Subtitle,
+  Card,
+  CardIcon,
+  CardText,
+  DiscoverMore,
+  ArrowIcon,
+} from '../elements/AboutElements';
 
 const About = () => {
   return (
     <AboutSection>
-      {/* Rest of the code remains the same... */}
-      <Card>
-        <CardIcon>
-          <FiSmartphone />
-        </CardIcon>
-        <CardText>Mobile Development</CardText>
-        <DiscoverMore>
-          Discover More <ArrowIcon>&rarr;</ArrowIcon>
-        </DiscoverMore>
-      </Card>
-      <Card>
-        <CardIcon>
-          <RiGlobeFill />
-        </CardIcon>
-        <CardText>Web Development</CardText>
-        <DiscoverMore>
-          Discover More <ArrowIcon>&rarr;</ArrowIcon>
-        </DiscoverMore>
-      </Card>
-      <Card>
-        <CardIcon>
-          <RiPencilFill />
-        </CardIcon>
-        <CardText>UI/UX Design</CardText>
-        <DiscoverMore>
-          Discover More <ArrowIcon>&rarr;</ArrowIcon>
-        </DiscoverMore>
-      </Card>
-      {/* Rest of the code remains the same... */}
+      <LeftContainer>
+        <Title>3+</Title>
+        <Subtitle>Years of Workings Experience</Subtitle>
+        <Description>
+          Mobile & Web Developer, specialized in client-side and functionality of applications, and designing, building, and maintaining the server side of web applications.
+        </Description>
+      </LeftContainer>
+      <RightContainer>
+        <Card>
+          <CardIcon>
+            <FiSmartphone />
+          </CardIcon>
+          <CardText>Mobile Development</CardText>
+          <DiscoverMore>
+            Discover More <ArrowIcon>&rarr;</ArrowIcon>
+          </DiscoverMore>
+        </Card>
+        <Card>
+          <CardIcon>
+            <RiGlobeFill />
+          </CardIcon>
+          <CardText>Web Development</CardText>
+          <DiscoverMore>
+            Discover More <ArrowIcon>&rarr;</ArrowIcon>
+          </DiscoverMore>
+        </Card>
+        <Card>
+          <CardIcon>
+            <RiPencilFill />
+          </CardIcon>
+          <CardText>UI/UX Design</CardText>
+          <DiscoverMore>
+            Discover More <ArrowIcon>&rarr;</ArrowIcon>
+          </DiscoverMore>
+        </Card>
+      </RightContainer>
     </AboutSection>
   );
 };
